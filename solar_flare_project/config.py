9a826c18251f8bb7aa17ec5a4ef68ea845338688
kaylie-a <kaguila@uccs.edu>
2025-10-24 21:59:46 -0600

    Create partition pairs and methods for dataset info

--- solar_flare_project/config.py
+++ solar_flare_project/config.py
@@ -1,3 +1,8 @@
 # Global variables/constants
 DATASET_DIR = 'C:/GitHub/solar-flare-project/data/raw/Cleaned SWANSF Dataset'
 NUM_PARTITIONS = 5
+NUM_TIMESTEPS = 60
+NUM_FEATURES = 24
+FEATURE_NAMES = ['R_VALUE', 'TOTUSJH', 'TOTBSQ', 'TOTPOT', 'TOTUSJZ', 'ABSNJZH', 'SAVNCPP', 'USFLUX',
+                 'TOTFZ', 'MEANPOT', 'EPSX', 'EPSY', 'EPSZ', 'MEANSHR', 'SHRGT45', 'MEANGAM', 'MEANGBT', 'MEANGBZ', 'MEANGBH',
+                 'MEANJZH', 'TOTFY', 'MEANJZD', 'MEANALP', 'TOTFX']
