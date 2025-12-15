03a88b5fe93dc9bb14883b7b9c79647a568d7ed9
kaylie-a <kaguila@uccs.edu>
2025-12-15 00:47:47 -0700

    Directory changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SOLAR FLARE PROJECT
+# SOLAR FLARE CLASSIFICATION
 
 Using a cleaned dataset with solar flare data, we analyzed the performance of two Transformer architectures in predicting solar flare levels across various time periods. Between a baseline time-series and PatchTST encoder, we compared how well each were able to analyze numerical data.
 
@@ -11,9 +11,10 @@ Using a cleaned dataset with solar flare data, we analyzed the performance of tw
 │
 ├── requirements.txt                   <- Install required libraries for environment
 │
-├── notebooks                          <- Code for running PatchTST
+├── pathTST                            <- Code for running PatchTST
+│   └── 0_5_JL_patchTST.ipynb
 │
-├── solar-flare-project                <- Code for running baseline Time-Series
+├── base_timeseries                    <- Code for running baseline Time-Series
 │   ├── base_time_series.py            <- Run training
 │   ├── config.py                      <- Configure local folder paths
 │   ├── load_dataset.py                <- Functions for working with the dataset
@@ -22,7 +23,7 @@ Using a cleaned dataset with solar flare data, we analyzed the performance of tw
 │
 ├── data\raw                           <- Original dataset
 │
-├── data_partitions                    <- Partition data after preprocessing (total of 10)
+├── data_partitions                    <- Partition data after preprocessing (total of 10), generated from base_timeseries\preprocessed_partition.py
 │   ├── test1.npz                      <- Testing partition 1
 │   ├── train1.npz                     <- Training partition 1
 │   └── . . .
@@ -44,4 +45,4 @@ The dataset used for this project is the SWAN-SF solar flare dataset.
 Original repository: [https://github.com/samresume/Cleaned-SWANSF-Dataset](https://github.com/samresume/Cleaned-SWANSF-Dataset)
 
 ## Local Setup
-Download the dataset and place it in: \solar-flare-project\data\raw
+Download the dataset and place it in: data\raw
--- base_timeseries/plot_graphs.py
+++ base_timeseries/plot_graphs.py
@@ -1,9 +1,10 @@
-from config import FIGURES_DIR, NUM_TIMESTEPS, FEATURE_NAMES
+from config import FIGURES_DIR, NUM_TIMESTEPS, NUM_FEATURES, FEATURE_NAMES
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 
+# Plot accuracy or loss during training
 def plot_history(metric, training, validation, training_pair, testing_pair, iteration):
     plt.figure(figsize=(7, 5))
     plt.plot(training, label=f'Training {metric}')
@@ -38,12 +39,11 @@ def plot_confusion_matrix(y_true, predictions, training_pair, testing_pair, iter
 # Plot time-series data for different features of one sample
 def plot_timeseries_data(sample, list_features, title, example_index=0):
     
-    num_features = len(list_features)
     timesteps = np.arange(NUM_TIMESTEPS)
 
     plt.figure(figsize=(7, 5))  # 10, 15 for full
 
-    for i in range(num_features):
+    for i in range(NUM_FEATURES):
         plt.plot(timesteps, sample[example_index,:,list_features[i]], label=FEATURE_NAMES[list_features[i]])
 
     plt.xlabel('Timesteps')
@@ -52,5 +52,27 @@ def plot_timeseries_data(sample, list_features, title, example_index=0):
     plt.legend()
     
     output_path = f'{FIGURES_DIR}/timeseries_example_small_{example_index}.png'
+    plt.savefig(output_path)
+    plt.close()
+
+# Compare feature data between two partitions
+def plot_feature_example(x_train, x_test, train_partition, test_partition, feature, num_examples):
+    feature_training = []
+    feature_testing = []
+
+    for i in range(num_examples):
+        for j in range(NUM_TIMESTEPS):
+            feature_training.append(x_train[train_partition][i, :, feature][j])
+            feature_testing.append(x_test[test_partition][i, :, feature][j])
+
+    plt.figure(figsize=(20, 10))
+    plt.plot(feature_training, label=f'Train {train_partition + 1} - {FEATURE_NAMES[feature]}')
+    plt.plot(feature_testing, label=f'Test {test_partition + 1} - {FEATURE_NAMES[feature]}')
+    plt.xlabel('Timesteps')
+    plt.ylabel('Values')
+    plt.title(f'Example')
+    plt.legend()
+
+    output_path = f'{FIGURES_DIR}/{FEATURE_NAMES[feature]}_train{train_partition + 1}_test{test_partition + 1}.png'
     plt.savefig(output_path)
     plt.close()
